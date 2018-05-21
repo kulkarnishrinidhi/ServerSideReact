6cc5b15cf64459560089ecf80a6c85eef013d75c
Shrinidhi <shrinidhisk369@gmail.com>
2018-05-21 14:57:17 +0530

    urgent commit

--- src/client/client.js
+++ src/client/client.js
@@ -1,8 +1,9 @@
 // Starter point for the client side application
-
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
 import Routes from './Routes';
 import reducers from './reducers';
 
@@ -22,7 +23,7 @@ const store = createStore(reducers, {}, applyMiddleware(thunk))
 ReactDOM.hydrate(
   <Provider store={store}>
     <BrowserRouter>
-      <Routes />
+      <div> { renderRoutes(Routes) } </div>
     </BrowserRouter>
   </Provider>
   , document.querySelector('#root'));
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+// to use async and await babel polyfill needs to be imported
+import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/Routes'
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 
@@ -9,9 +13,12 @@ app.use(express.static('public'));
 app.get('*', (req, res) => {
   //the reason we are creating here is to get data first
   const store = createStore();
-
+  
   // Some logic to initialize and load data into 
   // the store before we attemt to call renderer function
+  matchRoutes(Routes, req.path).map( ({ route }) =>  {
+    return route.loadData ? route.loadData() : null;
+  });
 
   res.send(renderer(req, store));
 });
